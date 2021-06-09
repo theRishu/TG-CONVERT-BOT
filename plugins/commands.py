9f98f752527f0a72c612adc72b8c549168cf2efa
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-09 19:23:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,8 +18,15 @@ START_BUTTONS = InlineKeyboardMarkup(
         ]]
     )
 
-my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
+HTART_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton(text="⚙️ Help", callback_data="help"),
+        InlineKeyboardButton(text="🤖 About", callback_data="about"),
+        ],[
+        InlineKeyboardButton(text="Close 🔐", callback_data="close")
+        ]]
+    )
+
 
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
@@ -27,7 +34,7 @@ async def start(c, m):
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
                          reply_to_message_id=m.message_id,
-                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("My Father 👨‍💻", url=my_father), InlineKeyboardButton("📌Support channel", url=support)]]))
+                         reply_markup=START_BUTTONS,
     logger.info(f"{m.from_user.first_name} used start command")
 
 
@@ -38,6 +45,7 @@ async def help(c, m):
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.HELP,
                          reply_to_message_id=m.message_id,
+                         replay_markup=HTART_BUTTONS,
                          parse_mode="markdown")
 
 
