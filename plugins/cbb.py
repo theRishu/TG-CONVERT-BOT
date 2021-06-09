5d4dcba866ccb094f5f5f951551be7bc816f9a2a
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-09 19:25:01 +0530

    Create cbb.py

---
+++ plugins/cbb.py
@@ -0,0 +1,57 @@
+import pyrogram
+
+from plugins.help_text import rename_cb, cancel_extract
+from plugins.rename_file import force_name
+from pyrogram import Client as pyrogram, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
+from pyrogram.errors import UserNotParticipant
+
+from script import script
+
+helpbutton = [[
+        InlineKeyboardButton(f'Channel', url="https://t.me/VKPROJECTS"),
+        InlineKeyboardButton(f'Support', url="https://t.me/VKP_BOTS")
+        ],[
+        InlineKeyboardButton(f'🤖 About', callback_data="about")
+    ]]
+
+aboutbutton = [[
+        InlineKeyboardButton(f'🤔 How To Use', callback_data="help"),
+        InlineKeyboardButton(f'Close 🔐', callback_data="close")
+    ]]
+
+
+@pyrogram.on_callback_query()
+async def cb_handler(bot, update):
+        
+    if "rename_button" in update.data:
+        await update.message.delete()
+        await force_name(bot, update.message)
+
+    elif update.data == "help":
+        await update.answer()
+        keyboard = InlineKeyboardMarkup(helpbutton)
+        await update.message.edit_text(
+            text=translation.HELP_USER,
+            reply_markup=keyboard,
+            disable_web_page_preview=True
+        )
+        return
+
+    elif update.data == "about":
+        await update.answer()
+        keyboard = InlineKeyboardMarkup(aboutbutton)
+        await update.message.edit_text(
+            text=translation.ABOUT_ME,
+            reply_markup=keyboard,
+            disable_web_page_preview=True
+        )
+        return
+
+    elif update.data == "close":
+        await update.message.delete()
+
+        
+    elif "cancel_e" in update.data:
+        await update.message.delete()
+        await cancel_extract(bot, update.message)
